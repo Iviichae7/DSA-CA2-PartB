c947adf7d3dd55f50c536060d02983093aecfa26
iviichae7 <iviichae7@gmail.com>
2024-03-14 19:25:24 +0000

    Updated GUI

--- Main/src/main/java/com/mycompany/main/Main.java
+++ Main/src/main/java/com/mycompany/main/Main.java
@@ -11,6 +11,8 @@ package com.mycompany.main;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        java.awt.EventQueue.invokeLater(() -> {
+            new GUIClient().setVisible(true);
+        });
     }
 }
